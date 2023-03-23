fe0d44bc21460e2a5f1b72c370cd35b7765e3488
taldoJonas1 <126732888+taldoJonas1@users.noreply.github.com>
2023-03-22 22:23:24 -0300

    Create lista1.js

---
+++ lista1.js
@@ -0,0 +1,52 @@
+function exe1(){
+    let n1 = Number (document.getElementById("n1").value)
+    let n2 = Number (document.getElementById("n2").value)
+    let sub = (n1 - n2)
+    alert ("A subtração é: " + sub)
+}
+
+function exe2(){
+    
+}
+
+function exe3(){
+    
+}
+
+function exe4(){
+    
+}
+
+function exe5(){
+    
+}
+
+function exe6(){
+    // let declara variavel
+    // recupera o salario fixo informado pelo usuario
+    let fixo = Number (document.getElementById("fixo").value)
+    // recupera vendas informadas na caixa de texto
+    let vendas = Number (document.getElementById("vendas").value)
+    
+    // calcular a comissão
+    let comissao = (vendas*0.04)
+    // calcular o salario final
+    let total = (comissao + fixo)
+    alert ("comissao " + comissao + "\nSalario total: " + total)
+}
+
+function exe7(){
+    
+}
+
+function exe8(){
+    
+}
+
+function exe9(){
+    
+}
+
+function exe10(){
+    
+}
